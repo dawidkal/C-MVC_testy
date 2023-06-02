c82e5b6b7ea75b740de78f2deef4adc25f85918a
dawidkal <48098684+dawidkal@users.noreply.github.com>
2023-06-02 20:39:36 +0200

    Add files via upload

---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SklepMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SklepMVC.ViewModels
+{
+    public class HomeViewModel
+    { // 3 listy na stone glowna
+        public IEnumerable <Kategoria> Kategorie{ get; set; }
+            
+        public IEnumerable<Kurs> Nowosci { get; set; }
+        public IEnumerable<Kurs> Bestsellery { get; set; }
+    }
+}
