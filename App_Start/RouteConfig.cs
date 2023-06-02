3019bc5dfe59e0092e757ed6a3647e254c92e4a9
dawidkal <48098684+dawidkal@users.noreply.github.com>
2023-06-02 20:32:17 +0200

    Add files via upload

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SklepMVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //trasa dla stron statycznych ze stopki
+            routes.MapRoute(
+                name: "StronyStatyczne",
+                url: "strona/{nazwa}.html",
+                defaults:new {controller ="Home", action="StronyStatyczne" });  //musi byc wpis w Webconfig
+
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
